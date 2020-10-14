725acd1c3215e5e0a9e6352c682d87023df34c79
ievaza <ievaza@users.noreply.github.com>
2020-10-14 19:22:45 +0300

    feature JS without tests

---
+++ js/components/feature/renderFeature.js
@@ -0,0 +1,23 @@
+import { generateSingleFeature } from "./generateSingleFeature.js";
+
+function renderFeature (data){
+
+    //input validation
+
+    //logic
+    let HTML = '';
+    const featuresDOM = document.querySelector(data.selector);
+    const featuresCount = data.features.length;
+  
+    for (let i = 0 ; i < featuresCount ; i++){
+        const feature = data.features[i];
+        HTML += generateSingleFeature(feature)
+    }
+
+    // post logic validation
+    
+    return featuresDOM.innerHTML = HTML;
+}
+
+export {renderFeature}
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+
 // header import
 // hero import
 // about import
-
+import {featureData} from './data/features.js';
+import {renderFeature} from './components/feature/renderFeature.js';
 
 // header logic
 // hero logic
 // about logic
+renderFeature(featureData)
