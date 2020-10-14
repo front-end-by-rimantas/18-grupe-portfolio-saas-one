c67a99260cf1d31bbcf41b69a2a5bd087dfe2674
rositatisor <70884246+rositatisor@users.noreply.github.com>
2020-10-14 23:05:24 +0300

    faq data

---
+++ js/data/faq.js
@@ -0,0 +1,45 @@
+const faqData = {
+    selector: 'faq_block', // nepamirsti ideti ID i HTML
+    faq: [
+        {
+            question: 'How would you integrate social media ad campaign?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        },
+        {
+            question: 'Projects that need to be turned around?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        },
+        {
+            question: 'How do you handle failure?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        },
+        {
+            question: 'Is there any custome pricing system?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        },
+        {
+            question: 'How would you integrate social media ad campaign?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        },
+        {
+            question: 'How would you integrate social media ad campaign?',
+            icon: 'fas fa-angle-down',
+            answer: 'Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Tellus integer feugiat scelerisque varius morbi enim nunc. Nulla aliquet enim tortor at auctor urna. Id semper risus in hendrerit gravida rutrum.',
+            active: true,
+        }
+
+    ]
+
+}
+
+export {faqData}
