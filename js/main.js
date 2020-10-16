4289a62c1371643aed785d5d127479d1389a874a
Matulanas <61939694+Matulanas@users.noreply.github.com>
2020-10-16 09:15:18 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// header import
+
+// about import
+import { homeAboutData } from './data/homeAboutData.js';
+import { renderHomeAbout } from './components/home-about/renderHomeAbout.js';
+
+// features import
+
+// projects import
+
+// faq import
+
+// testimonials import
+
+// our-team import
+
+// address import
+
+// footer import
+
+// background animations import
+
+
+// header logic
+
+// about logic
+renderHomeAbout(homeAboutData);
+
+// features logic
+
+// projects logic
+
+// faq logic
+
+// testimonials logic
+
+// our-team logic
+
+// address logic
+
+// footer logic
+
+// background animations logic
