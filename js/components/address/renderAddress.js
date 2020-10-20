59a6efefdd45482651105da9a58e4b0defb11302
PovilasZubrys <48654760+PovilasZubrys@users.noreply.github.com>
2020-10-20 09:55:44 +0300

    JS

--- js/components/address/renderAddress.js
+++ js/components/address/renderAddress.js
@@ -1,15 +1,36 @@
 import { renderSingleAddress } from './renderSingleAddress.js'
+import { renderSingleAddress1 } from './renderSingleAddress.js'
+import { renderSingleAddress2 } from './renderSingleAddress.js'
 
 function renderAddress(data) {
     let HTML = '';
-    const addressDOM = document.querySelector(data.selector)
-    const addressCount = data.address.length;
+    let HTML1 = '';
+    let HTML2 = '';
 
-    for (let i = 0; i < addressCount; i++) {
-        const address = data.address[i];
-        HTML += renderSingleAddress(address);
+    const addressDOM = document.querySelector(data.selector);
+    const addressCount = data.address.length;
+    
+    console.log(data.address);
+    if (data) {
+
     }
-    return addressDOM.innerHTML = HTML;
+    HTML += renderSingleAddress(address);
+    HTML1 += renderSingleAddress1(address);
+    HTML2 += renderSingleAddress2(address);
+    // for (let i = 0; i < addressCount; i++) {
+    //     var address = data.address[i];
+    //     if (i = 0) {
+    //         HTML += renderSingleAddress(address);
+    //     } else if (i = 1) {
+    //         HTML1 += renderSingleAddress1(address);
+    //     } else if (i = 2) {
+    //         HTML2 += renderSingleAddress2(address);
+    //     }
+    // }
+    console.log(HTML);
+    console.log(HTML1);
+    console.log(HTML2);
+    return addressDOM.innerHTML = HTML + HTML1 + HTML2;
 }
 
 export { renderAddress }
--- js/components/address/renderSingleAddress.js
+++ js/components/address/renderSingleAddress.js
@@ -1,9 +1,9 @@
 import { isValidAddress } from './isValidAddress.js'
 
 function renderSingleAddress(address) {
-    if (!isValidAddress(address)) {
-        return '';
-    }
+    // if (!isValidAddress(address)) {
+    //     return '';
+    // }
     const HTML = `<div class="adrs-box-wrap">
                 <div class="adrs-box">
                     <div class="adrs-icons">${address.icon}</div>
@@ -12,5 +12,40 @@ function renderSingleAddress(address) {
                     </div>
                 </div>`;
     return HTML;
+
+}
+function renderSingleAddress1(address) {
+    // if (!isValidAddress(address)) {
+    //     return '';
+    // }
+    const HTML1 = `<div class="adrs-box-wrap">
+                    <div class="adrs-box">
+                        <div class="adrs-icons">${address.icon}</div>
+                        <h5 class="adrs-box-titles">${address.title}</h5>
+                        <a href="#" class="adrs-contacts">${address.email}</a>
+                        <a href="#" class="adrs-contacts">${address.phone}</a>
+                    </div>
+                </div>`
+    return HTML1
+}
+function renderSingleAddress2(address) {
+    // if (!isValidAddress(address)) {
+    //     return '';
+    // }
+    const HTML2 = `<div class="adrs-box-wrap">
+                    <div class="adrs-box">
+                        <div class="adrs-icons">${address.icon}</div>
+                        <h5 class="adrs-box-titles">${address.title}</h5>
+                        <p class="adrs-paragraphs">${address.text}</p>
+                        <p class="adrs-social">
+                            <a href="#">${address.facebook}</a>
+                            <a href="#">${address.twitter}</a>
+                            <a href="#">${address.linkedin}</a>
+                        </p>
+                    </div>
+                </div>`
+    return HTML2
 }
 export { renderSingleAddress };
+export { renderSingleAddress1 };
+export { renderSingleAddress2 };
