6555d82dcbc7d9b2800602081aa8cf6a5ef7475d
IgnasKazenas <ignasliut@gmail.com>
2020-10-21 11:58:24 +0300

    object commit

--- js/components/header/HeaderNav.js
+++ js/components/header/HeaderNav.js
@@ -1,10 +1,21 @@
 class HeaderNav {
     constructor(params) {
         console.log(params);
+
+        this.DOM = document.querySelector(this.selector);
+
+        this.render()
     }
 
     generateHTML() {
+        const HTML = `
+            <a class="navbtn" href="#">Home.</a>
+        `
+        return HTML;
+    }
 
+    render() {
+        this.DOM.innerHTML = this.generateHTML();
     }
 }
 
