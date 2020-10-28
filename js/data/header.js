d1f34f3bfdcb5ca44b3b60299ce39e1d32236b09
ievaza <ievaza@users.noreply.github.com>
2020-10-28 18:34:54 +0200

    update stash

--- js/data/header.js
+++ js/data/header.js
@@ -1,100 +1,32 @@
 const headerData = {
-    selector: '#header_nav',
+    selector: '#header-navigation',
     headerMenu: [
         {
             title: 'Home.',
-            link: '#',
+
         },
         {
             title: 'Pages.',
-            link: '#',
-            dropdown: [
-                {
-                    title: 'Service.',
-                    link: '#',
-                },
-                {
-                    title: 'About',
-                    link: '#',
-                },
-                {
-                    title: 'Contact',
-                    link: '#',
-                },
-                {
-                    title: 'Pricing',
-                    link: '#',
-                },
-                {
-                    title: 'Case Study....',
-                    link: '#',
-                    dropdown: [
-                        {
-                            title: 'Case Default',
-                            link: '#',
-                        },
-                        {
-                            title: 'Case Detail',
-                            link: '#',
-                        }
-                    ]
-                },
-                {
-                    title: 'Our Team',
-                    link: '#',
-                },
-                {
-                    title: 'FAQ',
-                    link: '#',
-                },
-                {
-                    title: 'Terms of use',
-                    link: '#',
-                },
-                {
-                    title: 'ERROR 404',
-                    link: '#',
-                }
-            ]
+
         },
         {
             title: 'Blog.',
-            link: '#',
-            dropdown: [
-                {
-                    title: 'Blog Default',
-                    link: '#',
-                },
-                {
-                    title: 'Blog Single',
-                    link: '#',
-                }
-            ]
+
         },
         {
             title: 'Portfolio.',
-            link: '#',
-            dropdown: [
-                {
-                    title: 'Portfolio Standard',
-                    link: '#',
-                },
-                {
-                    title: 'Portfolio Masonry',
-                    link: '#',
-                }
-            ]
+
         },
         {
             title: 'About.',
+
             link: '#',
         },
         {
             title: 'Contact.',
+
             link: '#',
 
         }
     ]
 };
-
-export { headerData }
