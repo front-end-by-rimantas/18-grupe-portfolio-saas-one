ec0997f1e91f030fafc6b2b5b5e20e39e614f4b5
ievaza <ievaza@users.noreply.github.com>
2020-10-28 21:44:36 +0200

    counting projects

--- js/components/projects/Projects.js
+++ js/components/projects/Projects.js
@@ -40,26 +40,25 @@ class Projects {
         for (let num of this.data) {
 
             HTML += ` <div class="counter">
-                        <div class="proj-numb clr">${num.number}</div>
-                        <div class="proj-let clr">${num.amount}</div>
-                    </div>`
-        }
+                        <div class="proj-numb clr" style="color:${num.color}">${num.number}</div>
+                        <div class="proj-let clr" style="color:${num.color}">${num.amount}</div>
+                          <div ><p> ${num.title}</p> </div>
+                    </div >`
 
+        }
 
         this.DOM.insertAdjacentHTML(this.insertionPosision, HTML);
         this.projectsDOM = this.DOM.querySelectorAll(".counter");
-        this.projectsNumberDOM = this.DOM, querySelectorAll('.counter > .proj-numb')
-
-        console.log(this.projectsNumberDOM)
-        // KODEL NEREIKIA, NES KAI ATKOMENTUOTA META ERROR, BET ATVAIZDUOJA?
-
+        this.projectsNumberDOM = this.DOM.querySelectorAll('.counter > .proj-numb')
     }
 
     scrollAnimation() {
         const screenBottomY = scrollY + innerHeight;
 
+
         for (let i = 0; i < this.data.length; i++) {
             const num = this.projectsNumberDOM[i];
+
 
             if (num.dataset.run) {
                 continue;
@@ -70,16 +69,18 @@ class Projects {
                 num.dataset.run = true;
 
                 let currentValue = 0;
-                const finalValue = this.data[i].value;
+                const finalValue = this.data[i].number;
                 const timeAmount = 1;                   // seconds
                 const fps = 24;                         // frames per second
                 let step = 0;
                 const totalSteps = timeAmount * fps;
+                console.log(totalSteps)
 
                 const timer = setInterval(() => {
                     step++;
                     currentValue = Math.ceil(finalValue / totalSteps * step);
                     this.projectsNumberDOM[i].innerText = currentValue;
+
 
                     if (currentValue >= finalValue) {
                         clearInterval(timer);
--- js/data/projects.js
+++ js/data/projects.js
@@ -5,22 +5,26 @@ const projectsData = {
         {
             number: 20,
             amount: "K+",
-            title: "Total downloads"
+            title: "Total downloads",
+            color: "#8687f3"
         },
         {
             number: 13,
             amount: "M+",
-            title: "Total Show"
+            title: "Total Show",
+            color: "#ee9f76"
         },
         {
             number: 224,
             amount: "+",
-            title: "Total Projects"
+            title: "Total Projects",
+            color: "#e882ff"
         },
         {
             number: 53,
             amount: "M+",
-            title: "Total Cases"
+            title: "Total Cases",
+            color: "#f42ade"
         },
     ],
 }
